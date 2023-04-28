ec101c0c51aab686f07cd16ff34204e28be11769
LAPTOP-M7T10733\jub12 <jub1234jub@gmail.com>
2023-04-28 09:14:08 +0300

    Added in first preset bunch, blood colors

--- FriendCore/Presets.cs
+++ FriendCore/Presets.cs
@@ -3,13 +3,26 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using UnityEngine;
 
 namespace FriendCore
 {
     public static class Presets
     {
 
-        
+        /// <summary>
+        /// Preset colors, e.g. various blood colors.
+        /// </summary>
+        public static class Colors
+        {
+            public static Color infected = new Color(1f, 0.31f, 0f);
+            public static Color hiveblood = new Color(0.957f, 0.608f, 0.212f);
+            public static Color lifeblood = new Color(0f, 0.584f, 1f);
+            public static Color guacamole = new Color(0.66f, 0.93f, 0.34f);
+            public static Color machinery = new Color(0.5f, 0.5f, 0.5f);
+        }
+
+
 
     }
 }
